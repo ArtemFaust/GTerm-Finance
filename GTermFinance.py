25842c8d848aabcd3c4e2500321f02c24ee68548
Artem Bokanev <23515673+ArtemFaust@users.noreply.github.com>
2022-05-03 20:07:15 +0300

    graph add database add

--- GTermFinance.py
+++ GTermFinance.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 import threading
 import time
 import getkey
+import sqlite3
+import asciichartpy
 tickets = {}  # Список ссылок тикетов акций
 cache = []  # кэш значий акций предыдучей итерации
 buy_pr = 25
@@ -16,6 +18,9 @@ select = 1  # Select print table
 table = None
 print_st = False
 print_st2 = True
+figs = {}
+trend_period = '1d'
+fig_to_show = None
 
 # Color
 R = "\033[0;31;40m"  # RED
@@ -27,7 +32,8 @@ Y = '\033[0;33m'  # Yellow
 # Hat
 hat = str(B+"G"+R+'o'+Y+'o'+B+'g'+G+'l'+R+'e'+Y+" Finanse"+N+"\n Input settings: " +
           "|"+B+"s"+N+"ell (price>"+str(sell_pr)+"%max)|" + " |"+G+"b"+N+"uy (price<min+"+str(buy_pr)+"%)|" + Y + " d"+N + " - set default \n" +
-          ' Tickets config: '+'|'+Y+'A'+N+' - add new ticket|'+' |'+Y+'D'+N+' - delete ticket| |select table:' + Y+' 1, 2, 3'+N+'|')
+          ' Tickets config: '+'|'+Y+'A'+N+' - add new ticket|'+' |'+Y+'D'+N+' - delete ticket| |select view:' + Y+' 1, 2, 3'+N+'|'+Y+' T'+N+' - Trend period: %s ' % trend_period + '(1d, 5d, 1m)\n'+
+            Y+' G'+N+' - set ticket to graph')
 print(hat)
 # Считываем индексы из файла и формируем список ссылок
 
@@ -43,6 +49,17 @@ def getUrl():
     data.close()
 getUrl()
 
+# функция Выполнение sql запроса для получения полей тикетов
+def sql_execute(sql):
+    sqlite_connection = sqlite3.connect('ticket.db')
+    cursor = sqlite_connection.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    sqlite_connection.commit()
+    cursor.close()
+    sqlite_connection.close()
+    sqlite_connection = None
+    return rows
 
 # Заголовок эмуляции браузера
 headers = {
@@ -56,21 +73,22 @@ def refrasher():
     global hat
     hat = str(B+"G"+R+'o'+Y+'o'+B+'g'+G+'l'+R+'e'+Y+" Finanse"+N+"\n Input settings: " +
               "|"+B+"s"+N+"ell (price>"+str(sell_pr)+"%max)|" + " |"+G+"b"+N+"uy (price<min+"+str(buy_pr)+"%)|" + Y + " d"+N + " - set default \n" +
-              ' Tickets config: '+'|'+Y+'A'+N+' - add new ticket|'+' |'+Y+'D'+N+' - delete ticket| |select table:' + Y+' 1, 2, 3'+N+'|')
+              ' Tickets config: '+'|'+Y+'A'+N+' - add new ticket|'+' |'+Y+'D'+N+' - delete ticket| |select view:' + Y+' 1, 2, 3'+N+'|'+Y+' T'+N+' - Trend period: %s ' % trend_period + '(1d, 5d, 1m)\n' +
+              Y+' G'+N+' - set ticket to graph')
 
 
 # Функция обновления тикетов и построения таблици
 def update_ticker():
-    global cache, table, print_st, print_st
+    global cache, table, print_st, print_st, figs
     print_st = False
     # Формируем таблицу
     th = ["Name", "Price", "¤", "%",
-          "UpdateTime", 'RangePerYear', 'Action', "   ~  ", "DY"]
+          "UpdateTime", 'RangePerYear', 'Action', "   ~  ", "DY", ' 📈 TL 📉 ']
     table = PrettyTable(th)
     td = []
     # Получаем данные по ссылкам
     getUrl()
-    for i in tqdm(tickets, bar_format='{l_bar}{bar:80}'):
+    for i in tqdm(tickets, bar_format='{l_bar}{bar:100}'):
         # Получаем html код по ссылкам
         atempt = True # Переключатель попыток
         for _ in range(3): # 3 попытки получния данных
@@ -87,23 +105,45 @@ def update_ticker():
                     dividend_yield = Range_per_year[5].text  # Дивидендная доходность
                     currency = convert[0].text[0]  # тип валюты
                     price = convert[0].text[1:]  # Значение индекса
-                    
                     # Вставляем данные в массив данных для таблици
                     td.append([i, price, currency, '   %   ', "--:--",
-                            RangePerYear, "    *    ", "   ~  ", dividend_yield])
+                               RangePerYear, "    *    ", "   ~  ", dividend_yield, '📈🐄 or 🐻📉'])
                     atempt = False # Если данные получины прекращаем попытки по данному тикету
+                    # Встанвка данных в БД
+                    try:
+                        if "," in price:
+                            price = price.split(",")[0] + "." + \
+                                price.split(",")[1].split(".")[0]
+                        # Вставка данных по тикету в БД
+                        sqlite_connection = sqlite3.connect('ticket.db')
+                        cursor = sqlite_connection.cursor()
+                        sql = """INSERT INTO ticket_data (TicketName, Year, Month, Day, Hour, Minute, Second, IndexValue) VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7});""".format(
+                            "'"+str(i)+"'", "'"+str(datetime.datetime.now().year)+"'", "'"+str(
+                                            datetime.datetime.now().month)+"'", "'"+str(datetime.datetime.now().day)+"'",
+                            "'"+str(datetime.datetime.now().hour)+"'", "'"+str(datetime.datetime.now().minute)+"'", "'"+str(datetime.datetime.now().second)+"'", float(price))
+                        cursor.execute(sql)
+                        sqlite_connection.commit()
+                        cursor.close()
+                        sqlite_connection.close()
+                        sqlite_connection = None
+                    except Exception as ex:
+                        log = open('log.txt', 'a')
+                        log.write(str(type(ex)) + '\n' +
+                                  str(ex) + ' line 131' + '\n\n\n')
+                        log.close()
                 # Ошибка получения тикета удаляем его из файла data
                 except Exception as ex:
                     log = open('log.txt', 'a')
-                    log.write(str(type(ex)) + '\n' + str(ex) +' line 98'+ '\n\n\n')
+                    log.write(str(type(ex)) + '\n' + str(ex) +' line 109'+ '\n\n\n')
                     log.close()
                     td.append(['-', '-', '-', '   %   ', "--:--",
-                            '-', "    *    ", "   ~  ", '-'])
+                            '-', "    *    ", "   ~  ", '-', '-'])
     # Создаем таблицу
         table = PrettyTable(th)
         x = 0
+        cache = td
         for i in td:
-            if i[0] != '-':
+            if cache != [] and i[1] != '-' and cache[x][1].split("m")[0] != '-':
                 try:
                     if "," in i[1]:
                         i[1] = i[1].split(",")[0] + "." + i[1].split(",")[1].split(".")[0]
@@ -119,6 +159,7 @@ def update_ticker():
                             v = i[5].split(' ')[0].split('₽')[1]
                             if ',' in v:
                                 v = v.split(',')[0] + '.' + v.split(',')[1].split('.')[0]
+                            # Безусловный процент покупки
                             if float(i[1]) < float(v) or float(i[1]) >= float(v) and float(i[1]) < float(v) + ((float(v)*buy_pr)/100):
                                 i[6] = G+"!!!Buy!!!"+N
                                 i[7] = '~ '+str(float(v) + ((float(v)*buy_pr)/100))[0:6]
@@ -133,9 +174,40 @@ def update_ticker():
                             v = i[5].split(' ')[2].split('₽')[1]
                             if ',' in v:
                                 v = v.split(',')[0] + '.' + v.split(',')[1].split('.')[0]
+                            # Безусловный процент продажи
                             if float(i[1]) >= (float(v)*sell_pr)/100:
                                 i[6] = B+"!!!SELL!!!"+N
                                 i[7] = '~ '+str((float(v)*sell_pr)/100)[0:6]+i[2]
+
+                        # Определяем тренд
+                        # медвежий низходячий бычий восходячий тренд в зависимости от периода
+                        # Получаем данные из ДБ
+                        if trend_period == '1d':  # переиод 1 день
+                            sql = """select * from ticket_data where TicketName = '{0}' and Year = '{1}' and Month = '{2}' and Day = '{3}';""".format(
+                                str(i[0]), str(datetime.datetime.now().year), str(datetime.datetime.now().month), str(datetime.datetime.now().day))
+                            rows = sql_execute(sql)
+                            all_day_value_list = []
+                            for _ in rows:
+                                all_day_value_list.append(list(_)[7])
+
+                            top_list = [] # Максимумы дня
+                            min_list = [] # Минимумы дня
+                            top_list.append(all_day_value_list[0])
+                            min_list.append(all_day_value_list[0])
+                            k = 0
+                            for _ in all_day_value_list[1:]:
+                                if float(_) > float(top_list[0]) and float(_) > float(top_list[k]):
+                                    top_list.append(_)
+                                    k += 1
+                                elif float(_) < float(min_list[0]) and float(_) < float(min_list[k]):
+                                    min_list.append(_)     
+                                    k += 1
+                            top_list = top_list[1:] # Максимальные значения
+                            min_list = min_list[1:] # Минимальные значения
+                            
+                            # Формируем графики
+                            figs[i[0]] = [all_day_value_list, top_list, min_list]
+
                         table.add_row(i)
                     else:
                         i[6] = '    *    '
@@ -143,8 +215,8 @@ def update_ticker():
                     x += 1
                 except Exception as ex:
                     log = open('log.txt', 'a')
-                    log.write(str(type(ex)) + '\n' + str(ex) +' line 146'+ '\n\n\n')
-                    log.close()     
+                    log.write(str(type(ex)) + '\n' + str(ex) +' line 204'+ '\n\n\n')
+                    log.close()    
     print_st = True
     cache = td
     time.sleep(1.2)
@@ -153,7 +225,7 @@ def update_ticker():
 
 # User input thread
 def UserInput():
-    global sell_pr, buy_pr, select, hat
+    global sell_pr, buy_pr, select, hat, fig_to_show
     while True:
         key = getkey.getkey()
         match key:
@@ -209,11 +281,12 @@ def UserInput():
                 select = 2
             case '3':
                 select = 3
-
+            case 'G':
+                fig_to_show = input('Ticket?:').upper()
 
 # Selecter: Print table depending on select
 def printing():
-    global print_st, print_st2, table, hat
+    global print_st, print_st2, table, hat, figs, fig_to_show
     while True:
         time.sleep(0.5)
         match select:
@@ -229,9 +302,29 @@ def printing():
                     print(hat)
                     print('This is divident calendar in future')
                     print_st2 = False
-            case 3:  # Print ....
-                pass
-
+            case 3:  # Print fig
+                if print_st:
+                    os.system("clear")
+                    print(hat)
+                    try:
+                        config = {
+                            'colors': [
+                                asciichartpy.green,
+                                asciichartpy.magenta,
+                                asciichartpy.red
+                            ]
+                        }
+                        if fig_to_show == None:
+                            fig_to_show = list(figs.keys())[0]
+                        fig = figs[fig_to_show]
+                        print('Ticket: ' + Y+fig_to_show+N+' lenght: '+ str(len(fig[0])))
+                        print(asciichartpy.plot(
+                            series=[fig[0], fig[1], fig[2]], cfg=config))
+                    except Exception as ex:
+                        log = open('log.txt', 'a')
+                        log.write(str(type(ex)) + '\n' + str(ex) +' line 315'+ '\n\n\n')
+                        log.close() 
+                    print_st = False
 
 # Threads
 # User input thread
